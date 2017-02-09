fe3ee4ca5674b7b7390d6e4f7c7b34d1877a2b8e
echosteg <email@echosteg.com>
2017-02-09 22:51:47 +0530

    adding delete ability

---
+++ src/components/post_show.js
@@ -0,0 +1,58 @@
+import React, { Component,PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost, deletePost } from '../actions/index';
+
+import { Link } from 'react-router';
+
+class PostShow extends Component {
+
+    static contextTypes = {
+        router: PropTypes.object
+    };
+
+    componentWillMount(){
+        this.props.fetchPost(this.props.params.id);
+    }
+
+    onDeleteClick(id){
+        this.props.deletePost(id).then(()=>{
+            this.context.router.push('/');
+        });
+
+    }
+
+    render(){
+
+        const {post} = this.props;
+
+        const btnStyle = {
+            'marginRight': '5px'
+        }
+
+        if (!this.props.post){
+            return (
+                <div>
+                    loading...
+                </div>
+            );
+        }
+        return (
+            <div>
+                <Link className="btn btn-primary pull-xs-right" to="/" type="button">Back to Index</Link>
+                <button className="btn btn-danger pull-xs-right" style={btnStyle} onClick={()=>this.onDeleteClick(post.id)}>Delete Post</button>
+                <h3>{post.title}</h3>
+                <h6>Categories: {post.categories}</h6>
+                <p>{post.content}</p>
+            </div>
+
+        );
+    }
+}
+
+function mapStateToProps(state){
+    return {
+        post: state.posts.post
+    };
+}
+
+export default connect(mapStateToProps,{fetchPost, deletePost})(PostShow);
